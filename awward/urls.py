68a3b647a6982d4d75060664cb05064360b0214b
UMUTONIRitha <rithamarie9@gmail.com>
2021-01-19 07:49:44 +0200

    save changes

--- awward/urls.py
+++ awward/urls.py
@@ -4,11 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns =[
-    # url(r'^$', views.welcome, name = 'welcome'),
-    url(r'^$', views.index, name = 'index'),
-    url(r'^new/upload$',views.uploadproject,name = 'upload'),
-    url(r'^new/profile$', views.addprofile, name='edit'),
-    url(r'^myprofile$',views.userprofile,name ='myprofile'),
+    url(r'^$',views.index,name = 'index'),
+    url(r'^new/upload$', views.upload_project, name='upload'),
+    url(r'^new/profile$', views.add_profile, name='edit'),
+    url(r'^myprofile$',views.my_profile,name ='myprofile'),
     url(r'^oneproject/(\d+)$', views.one_project, name='oneproject'),
     url(r'^search/$', views.search_project, name='search'),
     url(r'^comment/(\d+)/$', views.add_comment, name='comment'),
@@ -16,6 +15,21 @@ urlpatterns =[
     url(r'^api/profile/$', views.ProfileList.as_view(), name='profile'),
     url(r'^api/project/$', views.ProjectList.as_view(), name='project'),
 ]
-
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
