ed7aee14320becd96e033b1b5023fd1ffa6bd6e7
UMUTONIRitha <rithamarie9@gmail.com>
2021-01-19 07:50:01 +0200

    save changes

--- awward/views.py
+++ awward/views.py
@@ -10,34 +10,27 @@ from rest_framework.views import APIView
 from .serializers import ProfileSerializer,ProjectSerializer
 # Create your views here.
 
-
-# def welcome(request):
-#     date = dt.date.today()
-#     return render(request, 'welcome.html', {"date": date})
-
-@login_required(login_url='accounts/login/')
 def index(request):
     ones_project = Project.objects.all()
     all_projects = Project.get_all_projects()
     return render(request, 'index.html', {"all_projects": all_projects, "ones_project":ones_project})
 
-
-@login_required(login_url='accounts/login/')
-def uploadproject(request):
+@login_required(login_url='/accounts/login/')
+def upload_project(request):
     current_user = request.user
     if request.method == 'POST':
-        form = NewProfileForm(request.POST, request.FILES)
+        form = NewProjectForm(request.POST, request.FILES)
         if form.is_valid():
             image = form.save(commit=False)
             image.user = current_user
             image.save()
         return redirect('index')
     else:
-        form = NewProfileForm()
-    return render(request, 'uploadprojects.html', {"form": form})    
+        form = NewProjectForm()
+    return render(request, 'upload_project.html', {"form": form}) 
 
 @login_required(login_url='accounts/login/')
-def addprofile(request):
+def add_profile(request):
     current_user = request.user
     profile = Profile.objects.filter(id = current_user.id)
     if request.method == 'POST':
@@ -49,21 +42,20 @@ def addprofile(request):
             return redirect('myprofile')
     else:
         form = NewProfileForm()
-    return render(request, 'changeprofile.html', {"form":form})    
-
+    return render(request, 'edit_profile.html', {"form":form})  
 
 @login_required(login_url='accounts/login/')
-def userprofile(request):
+def my_profile(request):
     current_user = request.user
     my_projects = Project.objects.filter(user = current_user)
-    userprofile = Profile.objects.filter(user = current_user).first
-    return render(request, 'profiles.html', {"my+projects": my_projects, "userprofile":userprofile})
-
+    my_profile = Profile.objects.filter(user = current_user).first
+    return render(request, 'profiles.html', {"my+projects": my_projects, "my_profile":my_profile})
 
 @login_required(login_url='accounts/login/')
 def one_project(request, id):
     ones_project = Project.objects.filter(id = id)
     all_ratings = Rating.objects.filter(project = id)
+
     if request.method == 'POST':
         form = VotingForm(request.POST)
         if form.is_valid():
@@ -72,7 +64,6 @@ def one_project(request, id):
             rate.project = id
             rate.save()
         return redirect('oneproject', id)
-
     else:
         form = VotingForm()
     calculate = Rating.objects.filter(project = id)
@@ -82,29 +73,29 @@ def one_project(request, id):
     average_usability = 0
     average_design = 0
     average_content = 0
-
     for x in calculate:
         usability.append(x.usability)
         design.append(x.design)
         content.append(x.content)
+
         if len(usability)>0 or len(design)>0 or len(content)>0:
             average_usability+= round(sum(usability)/len(usability))
             average_design+= round(sum(design)/len(design))
             average_content+= round(sum(content)/len(content))
-
         else:
+
             average_usability = 0.0
             average_design = 0.0
             average_content = 0.0
     return render(request,'project.html',{"ones_project":ones_project,"all_ratings":all_ratings,"form":form,"usability":average_usability,"design":average_design,"content":average_content})
-
 # @login_required(login_url='/accounts/login/')
+
 def search_project(request):
     if 'project_name' in request.GET and request.GET["project_name"]:
-        searched_term = request.GET.get("project_name")
+        search_term = request.GET.get("project_name")
         searched_project = Project.search_by_title(search_term)
         message = f"{search_term}"
-        return render(request,"search.html",{"message":message, "project": searched_project})
+        return render(request, "search.html",{"message":message,"project": searched_project})
     else:
         message = "You haven't searched for any term"
         return render(request, 'search.html',{"message":message})
@@ -124,9 +115,9 @@ def add_comment(request, proj_id):
             return redirect('index')
     else:
         form = CommentForm()
-    return render(request, 'commentform.html', {"form": form, "proj_id": proj_id})
+    return render(request, 'comment_form.html', {"form": form, "proj_id": proj_id})
 
-# @login_required(login_url='/accounts/login/')
+
 def comment(request, id):
     mycomments = Comments.objects.filter(commented_project = id).all()
     return render(request, 'comments.html', {"mycomments":mycomments})
@@ -136,9 +127,15 @@ class ProfileList(APIView):
         all_users = Profile.objects.all()
         serializers = ProfileSerializer(all_users, many=True)
         return Response(serializers.data)
-        
+
 class ProjectList(APIView):
     def get(self, request, format=None):
         all_projects = Project.objects.all()
         serializers = ProjectSerializer(all_projects, many=True)
         return Response(serializers.data)
+
+
+
+
+
+
